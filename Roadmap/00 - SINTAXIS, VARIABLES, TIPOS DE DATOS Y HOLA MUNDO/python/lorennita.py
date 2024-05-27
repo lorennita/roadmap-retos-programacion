1112f4ad4ee4010710423cd17d88ef830c7f4b44
lorennita <127861385+lorennita@users.noreply.github.com>
2024-05-27 13:33:05 +0200

    Create lorennita.py
    reto 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lorennita.py
@@ -0,0 +1,21 @@
+# https://python.org
+#comentario en 1 línea
+"""
+esto es un comentario en varias lineas
+"""
+
+'''
+esto es un comentario en varias lineas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MI_CONSTANT = "Mi constante" #por convencion porque no existen
+
+my_int = 3
+my_float = 1.5
+my_bool = True
+my_str = "Mi cadena de texto"
+
+print("Hola python")
